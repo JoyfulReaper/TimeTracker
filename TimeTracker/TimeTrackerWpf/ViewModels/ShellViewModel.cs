ef3399652849b133333f810dd414f9d1aa802c0d
Kyle Givler <xvegan88x@gmail.com>
2022-07-02 14:29:35 -0400

    Making WPF forms sucks.... Why did I decide to try to use WPF/XAML?

--- TimeTracker/TimeTrackerWpf/ViewModels/ShellViewModel.cs
+++ TimeTracker/TimeTrackerWpf/ViewModels/ShellViewModel.cs
@@ -3,10 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TimeTrackerWpf.ViewModels;
 
 public class ShellViewModel : Conductor<object>
 {
+    private readonly CategoryManagerViewModel _categoryVm;
+
+    public ShellViewModel(CategoryManagerViewModel categoryVm)
+    {
+        _categoryVm = categoryVm;
+        
+        Task.Run(async () =>
+        {
+            await ActivateItemAsync(_categoryVm, new CancellationToken());
+        });
+    }
 }
