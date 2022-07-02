7a2eab17fc41774abf6ea7110aa9d938739b9f94
Kyle Givler <xvegan88x@gmail.com>
2022-07-02 13:05:27 -0400

    Start setting up Caliburn.Micro

---
+++ TimeTracker/TimeTrackerWpf/Bootstrapper.cs
@@ -0,0 +1,67 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTrackerWpf.ViewModels;
+
+namespace TimeTrackerWpf;
+public class Bootstrapper : BootstrapperBase
+{
+    private SimpleContainer _container;
+    
+    public Bootstrapper()
+    {
+        Initialize();
+    }
+
+    protected override void Configure()
+    {
+        _container = new SimpleContainer();
+
+        _container.Singleton<IWindowManager, WindowManager>();
+        _container.Singleton<IEventAggregator, EventAggregator>();
+
+        GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
+    }
+
+  
+    ///////////////////////Setup DI////////////////////////////////
+    protected override object GetInstance(Type service, string key)
+    {
+        return _container.GetInstance(service, key);
+    }
+
+    protected override IEnumerable<object> GetAllInstances(Type service)
+    {
+        return _container.GetAllInstances(service);
+    }
+
+    protected override void BuildUp(object instance)
+    {
+        _container.BuildUp(instance);
+    }
+    ///////////////////////End Setup DI//////////////////////////////
+
+
+    
+    ///////////////////////Assembies to look for views in////////////
+    protected override IEnumerable<Assembly> SelectAssemblies()
+    {
+        return new[] { Assembly.GetExecutingAssembly() };
+    }
+
+
+    
+    protected override void OnStartup(object sender, System.Windows.StartupEventArgs e)
+    {
+        DisplayRootViewForAsync<ShellViewModel>();
+    }
+}
